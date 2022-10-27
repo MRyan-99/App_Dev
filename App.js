5cb205ee67bdcc59bdf12faca711aabdbaae5750
Malachi Ryan <rya17003@byui.edu>
2022-10-27 14:52:50 -0600

    implementing input screen for onetime password

--- App.js
+++ App.js
@@ -5,28 +5,31 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
-//import { Button } from 'react-native-paper';
-
-
-
 
 const AppStack = createNativeStackNavigator();
+const loggedInStates={
+  NOT_LOGGED_IN: 'NOT_LOGGED_IN',
+  LOGGED_IN: 'LOGGED_IN',
+  CODE_SENT: 'CODE_SENT'
+}
 
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [loggedInState, setLoggedInState] = React.useState(loggedInStates.NOT_LOGGED_IN);
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
-  const [phoneNumber, setPhoneNumber] = React.useState("")
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [oneTimePassword, setOneTimePassword] = React.useState(null);
+
 
    if (isFirstLaunch == true){
 return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
  
 );
-  }else if(isLoggedIn){
+  }else if(loggedInStates == loggedInStates.LOGGED_IN){
     return <Navigation/>
   }
-else{
+else if(loggedInState == loggedInStates.NOT_LOGGED_IN){
   return(<View>
     <TextInput style={styles.input}
     placeholderTextColor='#0000FF'
@@ -38,21 +41,32 @@ else{
     title='Send'
       style={styles.button}
       onPress={async()=>{
-
+        
         if(phoneNumber == ""){
           alert('You have no power here without a phone number!')
-          console.log('this fool did not enter a phone number')
+          console.log('this fool did not enter a phone number!')
+        }
+        else if(phoneNumber.length < 10){
+          alert('I am sorry, but your input needs to be a little longer, I cannot understand gibberish!')
+          console.log('This dolt thinks they can get away with too short of a number!')
+        }
+        else if(phoneNumber.length > 10){
+          alert('I am sorry, but your input needs to be a little shorter, I cannot understand gibberish!')
+          console.log('This dope seems to think too much information is ok!')
         }
         else{
-          console.log('As you wish, my liege')
-          alert("as you wish")
+          console.log('As you wish, my liege.')
+          alert("as you wish.")
         await fetch('https://dev.stedi.me/twofactorlogin/' +phoneNumber,
         {
          method:'POST', 
          headers:{
           'content-type':'application/text'
          }
-        } )
+        }
+        )
+        setLoggedInState(loggedInStates.CODE_SENT)
+
       }
       
     }
@@ -61,7 +75,32 @@ else{
       </View>)
 
   } 
+else if(loggedInState == loggedInStates.CODE_SENT){
+  return(
+  <View>
+    <TextInput style={styles.input}
+    placeholderTextColor='#0000FF'
+    placeholder='Code'
+    value={oneTimePassword}
+    onChangeText={setOneTimePassword}>
+    </TextInput>
+    <Button
+    title='Confirm'
+    style={styles.button}
+    onPress={async()=>{
+      if(oneTimePassword == ""){
+        alert('You did not give me the password, you must now ask again!')
+        console.log('sending this dolt back to the phone number screed')
+        setLoggedInState(loggedInStates.NOT_LOGGED_IN)
+      }
+      
+      }}>      
+    </Button>
+  </View>
+  )
 }
+}
+
  export default App;
 
 const styles = StyleSheet.create({
